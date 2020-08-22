8c0649676868397839d3540d47e777d445c5109c
Bria White <bria.awhite@gmail.com>
2020-08-22 19:12:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(sentence)
+<<<<<<< HEAD
  sentence.split.collect{|word| word.reverse}.join(" ")
+=======
+ split_sentence = sentence.split(" ")
+ puts reverse_each_word
+>>>>>>> 1493714feac91afbc936055808d3621b52ef5e88
 end 
